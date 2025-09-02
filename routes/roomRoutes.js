88b361d3f16bed8aabcd50a651a466eb36e7b1aa
MuhammadAnas567 <anuszia445@gmail.com>
2025-09-02 19:14:40 +0100

    Joi validations

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -11,7 +11,12 @@ import upload from "../middlewares/uploadMiddleware.js";
 import { authenticateUser, authRole } from "../middlewares/authMiddleware.js";
 
 const router = Router();
+const express = require("express");
+const roomController = require("../controllers/roomController");
+const { createRoomSchema } = require("../validations/roomValidation");
+const validate = require("../middlewares/validate");
 
+router.post("/", validate(createRoomSchema), roomController.createRoom);
 router.post("/", authenticateUser, authRole(["admin"]), upload.array("images",10), createRoom);
 router.put("/:id", authenticateUser, authRole(["admin"]), upload.array("images",10), updateRoom);
 router.delete("/:id", authenticateUser, authRole(["admin"]), deleteRoom);
@@ -20,4 +25,5 @@ router.get("/", getAllRooms);
 router.get("/:id/update", getRoomById);
 router.get("/:id/status", getRoomStatus);
 
+module.exports = router;
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,10 +11,18 @@ import {
 import { authenticateUser, authRole } from "../middlewares/authMiddleware.js";
 
 const router = Router();
+const express = require("express");
+const userController = require("../controllers/userController");
+const { registerSchema, loginSchema } = require("../validations/userValidation");
+const validate = require("../middlewares/validate");
+
 
 router.post("/register", register);
 router.post("/admin/register", authenticateUser, authRole(["admin","receptionist"]), register);
 router.post("/login", login);
+router.post("/register", validate(registerSchema), userController.register);
+router.post("/login", validate(loginSchema), userController.login);
+
 
 router.get("/profile/me", authenticateUser, getUserProfile);
 
@@ -23,5 +31,7 @@ router.get("/:id", authenticateUser, authRole(["admin"]), getUserById);
 router.put("/:id", authenticateUser, authRole(["admin"]), updateUser);
 router.delete("/:id", authenticateUser, authRole(["admin"]), deleteUser);
 
+
+module.exports = router;
 
 export default router;
